d20aee7e87b86a7568e16dbe582aef89ef5a94ab
jeremysr <jeremysr@protonmail.com>
2023-07-31 12:12:22 -0600

    added some ids to the form/submit element. added the test fetch API calls to a function, and added that funtion to event listener on the form/submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,40 @@
 const apiKey = "590f0512a60e033b915bf85a1436336f";
-let latitude;
-let longitude;
-let cityName = "london";
+let cityText = $("#cityText");
+let submitForm = $("#submitForm");
 //const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-const geoCodeAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
 
 
-fetch(geoCodeAPI)
-.then(function(response) {
-    console.log(response);
-    return response.json();
-})
-.then(function(data) {
-    console.log(data);
-    latitude = data[0].lat
-    longitude = data[0].lon; 
-    console.log(`latitude is ${latitude}`);
-    console.log(`longitude is ${longitude}`);
-    const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-    fetch(apiURL)
+//event listener for the submit button. submit the input city to openWeatherAPI, get back the lat/long
+//then submit that to the 5 day forecast API, and display the results
+submitForm.on("submit", function(event) {
+    event.preventDefault();
+    fetchAllTheThings();
+});
+
+
+function fetchAllTheThings () {
+    cityText.val().trim();
+    let cityName = cityText.val();   
+    const geoCodeAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+    fetch(geoCodeAPI)
     .then(function(response) {
         console.log(response);
         return response.json();
     })
-    .then(function(data){
+    .then(function(data) {
         console.log(data);
+        let latitude = data[0].lat
+        let longitude = data[0].lon; 
+        console.log(`latitude is ${latitude}`);
+        console.log(`longitude is ${longitude}`);
+        const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+        fetch(apiURL)
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
     })
-})
+}
